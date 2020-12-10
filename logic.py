83d492d1354896294a6552251d7923df6c8a8fe6
jprik <60302563+jprik@users.noreply.github.com>
2020-12-10 16:08:05 -0700

    Stable build, simulation of 2019 Canadian General Election, featuring rejected ballots and all parties excluding the Bloc.

--- logic.py
+++ logic.py
@@ -6,27 +6,33 @@ class Party:
         self.weight = weight
         self.votes = 0
 
+# Console text
+print("\bModel of Canadian 2019 General Election:\n")
 # Establishing variables
-LIBERAL_PARTY = Party("Liberal", 3)
-CONSERVATIVE_PARTY = Party("Conservative", 2)
-NDP_PARTY = Party("NDP", 1)
 
-partyList = [LIBERAL_PARTY, CONSERVATIVE_PARTY, NDP_PARTY]
+LP= Party("Liberal Party of Canada", 4)
+CP = Party("Conservative Party of Canada", 3)
+NDP = Party("New Democratic Party of Canada", 2)
+GRN = Party("Green Party of Canada", 1)
+PPC = Party("People's Party of Canada", 1)
+X = Party("Refused Ballots", 1)
+
+partyList = [LP, CP, NDP, GRN, PPC, X]
+
 
 class Citizen:
     def __init__(self):
-        self.preference = random.choices(partyList, weights=[LIBERAL_PARTY.weight, CONSERVATIVE_PARTY.weight, NDP_PARTY.weight])
+        self.preference = random.choices(partyList, weights=[LP.weight, CP.weight, NDP.weight, GRN.weight, PPC.weight, X.weight])
 
 # Population who may vote
-population = 100
+population = 30
 
 def popularity_handler(x):
-    x = "{:.0%}".format(x)
-    return x
+    return "{:.0%}".format(x/population)
 
 
-def decimalize(x):
-    return x / population
+#def decimalize(x):
+    #return x / population
 
 # Recording citizen classes choice and then counting it
 
@@ -36,16 +42,17 @@ for i in range(0, population):
 for i in range(0, population):
     voteList[i].preference[0].votes += 1
 
-# Debug
-
-# print(voteList)
-# print(LIBERAL_PARTY, CONSERVATIVE_PARTY, NDP_PARTY)
+# Printing results + percentages and totals
 
 for i in range(0, len(partyList)):
-    print(partyList[i].name, partyList[i].votes)
+    print(f"{partyList[i].name}, {popularity_handler(partyList[i].votes)} | {partyList[i].votes} votes cast")
+# print("Total Votes:", total, "|", popularity_handler(total/population) + " Turnout")
+
+total = LP.votes + CP.votes + NDP.votes + GRN.votes + PPC.votes
+
+print(f"Total Votes Cast: {total}")
 
 
-# Printing results + percentages and totals
 # print("Liberal Party of Canada:", LIBERAL_PARTY, "votes,", str(popularity_handler(decimalize(LIBERAL_PARTY))))
 # print("Conservative Party of Canada:", CONSERVATIVE_PARTY, "votes,", str(popularity_handler(decimalize(CONSERVATIVE_PARTY))))
 # print("New Democratic Party of Canada:", NDP_PARTY, "votes,", str(popularity_handler(decimalize(NDP_PARTY))))
