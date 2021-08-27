13c11c5e8a5222f85162897f7201b7ded97d42c3
jusipat <justin10patrick@gmail.com>
2021-08-27 14:26:43 -0600

    Partially added Riding logic

--- logic.py
+++ logic.py
@@ -1,41 +1,56 @@
 import random
 
+# Variables
+population = 30
+ridingIterator = 1
+numberOfRidings = int(input("How many ridings exist in this election?"))
+title = '\bModel of Canadian 2019 General Election:\n'
+
+
 class Party:
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
         self.votes = 0
 
-# Console text
-print("\bModel of Canadian 2019 General Election:\n")
-# Establishing variables
 
-LP= Party("Liberal Party of Canada", 4)
-CP = Party("Conservative Party of Canada", 3)
+def popularity_handler(x):
+    return "{:.0%}".format(x / population)
+
+
+class Riding:
+    def __init__(self, name, result):
+        self.name = name
+        self.result = result
+
+
+LPC = Party("Liberal Party of Canada", 4, )
+CPC = Party("Conservative Party of Canada", 3)
 NDP = Party("New Democratic Party of Canada", 2)
 GRN = Party("Green Party of Canada", 1)
 PPC = Party("People's Party of Canada", 1)
 X = Party("Refused Ballots", 1)
 
-partyList = [LP, CP, NDP, GRN, PPC, X]
+partyList = [LPC, CPC, NDP, GRN, PPC, X]
+
+for i in range(0, numberOfRidings):
+    ridingInstance = Riding("Federal Riding " + str(ridingIterator), popularity_handler(partyList[i].votes))
+    ridingIterator += 1
+    print(ridingInstance.name, "has elected", ridingInstance.result)
+
+# Console text
+
+print(title)
 
 
+# Establishing variables
 class Citizen:
     def __init__(self):
-        self.preference = random.choices(partyList, weights=[LP.weight, CP.weight, NDP.weight, GRN.weight, PPC.weight, X.weight])
+        self.preference = random.choices(partyList,
+                                         weights=[LPC.weight, CPC.weight, NDP.weight, GRN.weight, PPC.weight, X.weight])
 
-# Population who may vote
-population = 30
-
-def popularity_handler(x):
-    return "{:.0%}".format(x/population)
-
-
-#def decimalize(x):
-    #return x / population
 
 # Recording citizen classes choice and then counting it
-
 voteList = []
 for i in range(0, population):
     voteList.append(Citizen())
@@ -43,18 +58,21 @@ for i in range(0, population):
     voteList[i].preference[0].votes += 1
 
 # Printing results + percentages and totals
-
 for i in range(0, len(partyList)):
     print(f"{partyList[i].name}, {popularity_handler(partyList[i].votes)} | {partyList[i].votes} votes cast")
-# print("Total Votes:", total, "|", popularity_handler(total/population) + " Turnout")
+total = [LPC.votes, CPC.votes, NDP.votes, GRN.votes, PPC.votes]
+print(f"Total Votes Cast: {sum(total)}")
 
-total = LP.votes + CP.votes + NDP.votes + GRN.votes + PPC.votes
+if max(total) == LPC.votes:
+    victor = LPC
+elif max(total) == CPC.votes:
+    victor = CPC
+elif max(total) == NDP.votes:
+    victor = NDP
+elif max(total) == GRN.votes:
+    victor = GRN
+elif max(total) == PPC.votes:
+    victor = PPC
 
-print(f"Total Votes Cast: {total}")
-
-
-# print("Liberal Party of Canada:", LIBERAL_PARTY, "votes,", str(popularity_handler(decimalize(LIBERAL_PARTY))))
-# print("Conservative Party of Canada:", CONSERVATIVE_PARTY, "votes,", str(popularity_handler(decimalize(CONSERVATIVE_PARTY))))
-# print("New Democratic Party of Canada:", NDP_PARTY, "votes,", str(popularity_handler(decimalize(NDP_PARTY))))
-# total = LIBERAL_PARTY + CONSERVATIVE_PARTY + NDP_PARTY
-# print("Total Votes:", total, "|", popularity_handler(total/population) + " Turnout")
+print(victor.name, f'has received the majority of the vote ({max(total)} votes) and has won in {ridingInstance.name}.')
+ridingIterator += 1
